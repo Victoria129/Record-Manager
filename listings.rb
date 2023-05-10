7a512036c0aa42ada0a8b7fcfd00e45ca66d29d6
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-05-10 13:17:51 +0300

    Fix linter errors

--- listings.rb
+++ listings.rb
@@ -2,25 +2,32 @@ class Listings
   def list_all_books
       puts 'These are all the Books'
   end
+
   def list_all_movies
       puts 'These are all the Movies'
   end
+
   def list_all_albums
       puts 'These are all the Music Albums'
   end
+
   def list_all_games
-      puts 'These are all the Games'
+    puts 'These are all the Games'
   end
+
   def list_all_genres
-      puts 'These are all the Genres'
+    puts 'These are all the Genres'
   end
+
   def list_all_labels
-      puts 'These are all the Labels'
+    puts 'These are all the Labels'
   end
+
   def list_all_authors
-      puts 'These are all the Authors'
+    puts 'These are all the Authors'
   end
+
   def list_all_sources
-      puts 'These are all the Sources'
+    puts 'These are all the Sources'
   end
 end
