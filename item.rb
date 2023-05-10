3a9d2ea1096361c2143381d8ab61dd0d4f361f21
Frempong <samankwaa.frempong@gmail.com>
2023-05-10 08:52:14 +0000

    Add methods to Item class

--- item.rb
+++ item.rb
@@ -11,4 +11,12 @@ class Item
     @publish_date = Date.parse(publish_date)
     @archived = archived
   end
+
+  def can_be_archived?
+    Date.today.year - @publish_date.year > 10 ? true : false
+  end
+
+  def move_to_archive
+    can_be_archived? ? @archived = true : @archived = false
+  end
 end
